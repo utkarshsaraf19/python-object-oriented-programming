c3198d967f41da1e6351f3954083f94e8dfb4daa
utkarsh.saraf <utkarsh.saraf19@gmail.com>
2021-04-07 14:19:18 +0530

    module renaming and comments

--- ecommerce_module/__init__.py
+++ ecommerce_module/__init__.py
@@ -1,2 +1,6 @@
+
+"""
+single dot mean same package level
+"""
 from .database import Database
 from .payments_module.stripe import Stripe
--- ecommerce_module/payments_module/__init__.py
+++ ecommerce_module/payments_module/__init__.py
@@ -1,2 +1,6 @@
+"""
+single dot mean same module level
+double dot means going one level parent to current module level
+"""
 from ..products import Products
 from .stripe import  Stripe
