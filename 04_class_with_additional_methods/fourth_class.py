b4f1d57a96cbc88c5f43e23bbcaf8bc1e4b15dbd
utkarshsaraf19 <utkarsh.saraf19@gmail.com>
2021-04-07 11:25:45 +0530

    simple class

---
+++ 04_class_with_additional_methods/fourth_class.py
@@ -0,0 +1,12 @@
+import math
+
+class Point:
+    def move(self,x,y):
+        self.x = x
+        self.y = y
+
+    def reset(self):
+        self.move(0,0)
+
+    def calculate_difference(self, another_point):
+        return math.sqrt(((self.x - another_point.x) ** 2) + ((self.y - another_point.y) ** 2))
