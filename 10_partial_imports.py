c2f00a11ec364effd105ce69765f1453106a8fb5
utkarsh.saraf <utkarsh.saraf19@gmail.com>
2021-04-07 19:27:58 +0530

    changes

--- 10_partial_imports.py
+++ 10_partial_imports.py
@@ -3,7 +3,9 @@ this became possible due to relative import __init__.py entry, direct method cal
 """
 
 from ecommerce_module import Database
+
 db2 = Database()
 
 from ecommerce_module.payments_module import Products
+
 prod = Products()
--- ecommerce_module/payments_module/__init__.py
+++ ecommerce_module/payments_module/__init__.py
@@ -3,4 +3,4 @@ single dot mean same module level
 double dot means going one level parent to current module level
 """
 from ..products import Products
-from .stripe import  Stripe
+from .stripe import Stripe
