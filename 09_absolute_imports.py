43bccab4d26f47122a6daee052ce8d68e3d39dd2
utkarsh.saraf <utkarsh.saraf19@gmail.com>
2021-04-07 14:15:38 +0530

    module renaming

--- 09_absolute_imports.py
+++ 09_absolute_imports.py
@@ -3,16 +3,16 @@ absolute imports
 modules are just python files and
 __init__.py gives it the identity of package rather than just folders
 """
-import ecommerce.database
-db_object = ecommerce.database.Database()
+import ecommerce_module.database
+db_object = ecommerce_module.database.Database()
 
-from ecommerce.database import Database
+from ecommerce_module.database import Database
 db_object1 = Database()
 
-from ecommerce import database
+from ecommerce_module import database
 db_object2 = database.Database()
 
-from ecommerce.database import Database as db
+from ecommerce_module.database import Database as db
 db_object3 = db()
 db_object3.x = 80
 db_object3.y = 90
--- 10_partial_imports.py
+++ 10_partial_imports.py
@@ -2,8 +2,8 @@
 this became possible due to relative import __init__.py entry, direct method calling
 """
 
-from ecommerce import Database
+from ecommerce_module import Database
 db2 = Database()
 
-from ecommerce.payments import Products
+from ecommerce_module.payments_module import Products
 prod = Products()
